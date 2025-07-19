4ba045c56f605590306fbc5cb9c1410118a1b78b
Barrett <barrett@bear.plus>
2025-07-19 10:59:07 +0000

    Latest change for Netlify serverless

---
+++ netlify/functions/api.ts
@@ -0,0 +1,11 @@
+import express, { Router } from 'express';
+import serverless from 'serverless-http';
+
+const app = express();
+const router = Router();
+
+router.get('/hello', (req, res) => res.json({ message: 'Hello World!' }));
+
+app.use('/api', router);
+
+export const handler = serverless(app);
